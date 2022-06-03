f90b0db59182d30806e6d52dd856832ece3f2287
Jakob Schlyter <jakob@kirei.se>
2022-06-03 21:45:32 +0200

    use pkcs#11

--- bind9/entrypoint.sh
+++ bind9/entrypoint.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-usermod -G softhsm named
-
 if [ ! -f /var/lib/softhsm/.initialized ]; then
-	sudo -u named softhsm2-util --init-token --free --label named --pin 1234 --so-pin 1234567890
+	sudo -u bind softhsm2-util --init-token --free --label named --pin 1234 --so-pin 1234567890
 	touch /var/lib/softhsm/.initialized
 fi
 
-if [ ! -f /etc/rndc.key ]; then
-	rndc-confgen -a
-	chown named /etc/rndc.key
-	chmod g+r /etc/rndc.key
-fi
-
 if [ ! -d /storage/keys ]; then
-	install -d -g named -m 770 /storage/keys
+	install -d -g bind -m 770 /storage/keys
+	sudo -u bind pkcs11-tool \
+		--module /usr/lib/softhsm/libsofthsm2.so \
+		--login --token-label named --pin 1234 \
+		--keypairgen --key-type EC:prime256v1 --label ksk1 
+	sudo -u bind pkcs11-tool \
+		--module /usr/lib/softhsm/libsofthsm2.so \
+		--login --token-label named --pin 1234 \
+		--keypairgen --key-type EC:prime256v1 --label zsk1 
+	sudo -u bind dnssec-keyfromlabel -E pkcs11 -K /storage/keys \
+		-a ECDSAP256SHA256 \
+		-l "token=named;object=ksk1;pin-value=1234" \
+		-f KSK \
+		example.com
+	sudo -u bind dnssec-keyfromlabel -E pkcs11 -K /storage/keys \
+		-a ECDSAP256SHA256 \
+		-l "token=named;object=ksk1;pin-value=1234" \
+		example.com
 fi
 
-#named -E pkcs11 -g
-named -g -c /config/named.conf
+chown bind:bind /config
+chown bind:bind /storage
+
+named -E pkcs11 -u bind -g -c /config/named.conf
