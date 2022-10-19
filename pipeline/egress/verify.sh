99924129f3e12fd7e736f6acec9b2525aee58675
Jakob Schlyter <jakob@kirei.se>
2022-10-19 11:18:35 +0200

    better verify script require ZONEMD

--- pipeline/egress/verify.sh
+++ pipeline/egress/verify.sh
@@ -6,10 +6,32 @@ ZONEFILE=/tmp/${ORIGIN}.zone
 rm -f $ZONEFILE
 cat > $ZONEFILE
 
-ldns-verify-zone -V 5 < $ZONEFILE && \
-kzonecheck --origin $ORIGIN --dnssec on --verbose $ZONEFILE && \
+echo Running ldns-verify-zone
+ldns-verify-zone -V 5 -ZZ < $ZONEFILE
+if [ $? -ne 0 ]; then
+	echo "ldns-verify-zone failed"
+	exit 1
+else
+	echo "ldns-verify-zone succeeded"
+fi
+
+echo Running kzonecheck
+kzonecheck --origin $ORIGIN --dnssec on --verbose $ZONEFILE
+if [ $? -ne 0 ]; then
+	echo "kzonecheck failed"
+	exit 2
+else
+	echo "kzonecheck succeeded"
+fi
+
+echo Running kzonecheck
 dnssec-verify -o $ORIGIN -x $ZONEFILE
+if [ $? -ne 0 ]; then
+	echo "dnssec-verify failed"
+	exit 3
+else
+	echo "dnssec-verify succeeded"
+fi
 
-ret=$?
-
-exit $res
+echo "zone verified successfully"
+exit 0
